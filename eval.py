c7b487b99daca6d906a9538b8a402b46e7251fc5
Jannik Pfeifer <s8japfei@stud.uni-saarland.de>
2018-06-07 16:15:38 +0200

    Removed functionality to rebuild AnalysisResult objects

--- eval.py
+++ eval.py
@@ -15,13 +15,6 @@ target_sdk_too_low = 0
 no_permissions_declared = 0
 
 
-def analyze_results(directory):
-    for file in os.listdir(directory):
-        if file.endswith(".json"):
-            with open(file, "r") as json_dict:
-                eval_apk(json.loads(json_dict.read()))
-
-
 def analyze_results_from_json(directory):
     for file in os.listdir(directory):
         if file.endswith(".json"):
@@ -33,43 +26,6 @@ def analyze_results_from_json(directory):
                 eval_single_analysis_result_from_json(json.loads(json_dict.read()))
             except:
                 print("Could not load %s" % file)
-
-
-def eval_apk(json):
-    analysis = Analysis()
-
-    a,d,dx = AnalyzeAPK(json["apk_path"])
-
-    for vm in d:
-        analysis.add(vm)
-
-    analysis.create_xref()
-
-    apk = AnalyzedApk.from_json(analysis, json)
-    eval_single_analysis_result(apk)
-
-
-def eval_single_analysis_result(analyzed_apk):
-    # type: (AnalyzedApk) -> ()
-
-    global analyzed_apks
-    analyzed_apks += 1
-
-    if not analyzed_apk.is_analyzable():
-        global not_analyzable_apks
-        not_analyzable_apks += 1
-
-        if analyzed_apk.error:
-            global analysis_errors
-            analysis_errors += 1
-
-        if analyzed_apk.target_sdk_too_low:
-            global target_sdk_too_low
-            target_sdk_too_low += 1
-
-        if analyzed_apk.no_permission_declared:
-            global no_permissions_declared
-            no_permissions_declared += 1
 
 
 def eval_single_analysis_result_from_json(analyzed_apk):
