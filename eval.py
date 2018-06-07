0194a9b0297f962d447946fea95c421ed088d53d
Jannik Pfeifer <s8japfei@stud.uni-saarland.de>
2018-06-07 08:08:51 +0200

    now uses pure json to evaluate

--- eval.py
+++ eval.py
@@ -1,10 +1,11 @@
-from .AnalysisUtils.analysis_result import AnalyzedApk
-from .AnalysisUtils.RequestAnalysisResult import RequestAnalysis
-from .AnalysisUtils.UsageAnalysisResult import UsageAnalysis
-from .AnalysisUtils.AndroguardProject.androguard.androguard.core.analysis.analysis import Analysis
-from .AnalysisUtils.AndroguardProject.androguard.androguard.misc import AnalyzeAPK
+from AnalysisUtils.analysis_result import AnalyzedApk
+from AnalysisUtils.RequestAnalysisResult import RequestAnalysis
+from AnalysisUtils.UsageAnalysisResult import UsageAnalysis
+from AnalysisUtils.AndroguardProject.androguard.androguard.core.analysis.analysis import Analysis
+from AnalysisUtils.AndroguardProject.androguard.androguard.misc import AnalyzeAPK
 import os
 import sys
+import json
 
 analyzed_apks = 0
 
@@ -18,10 +19,20 @@ def analyze_results(directory):
     for file in os.listdir(directory):
         if file.endswith(".json"):
             with open(file, "r") as json_dict:
-                eval_apk_from_json(json_dict)
+                eval_apk(json.loads(json_dict.read()))
 
 
-def eval_apk_from_json(json):
+def analyze_results_from_json(directory):
+    for file in os.listdir(directory):
+        if file.endswith(".json"):
+            json_dict = open(os.path.realpath(directory) + '/' + file, "r")
+            # For performance reasons directly evaluate from the json dict
+            # With this we skip invoking androguard again for obtaining an analysis instance
+            # But we also cannot use the classes of our actual framework but need to work directly with json dicts
+            eval_single_analysis_result_from_json(json.loads(json_dict.read()))
+
+
+def eval_apk(json):
     analysis = Analysis()
 
     a,d,dx = AnalyzeAPK(json["apk_path"])
@@ -58,6 +69,32 @@ def eval_single_analysis_result(analyzed_apk):
             no_permissions_declared += 1
 
 
+def eval_single_analysis_result_from_json(analyzed_apk):
+
+    global analyzed_apks
+    analyzed_apks += 1
+
+    invalid_target_sdk =  analyzed_apk["target_sdk_too_low"]
+    no_permission = analyzed_apk["no_permission"]
+    error = analyzed_apk["error"]
+
+    if invalid_target_sdk or no_permission or error:
+        global not_analyzable_apks
+        not_analyzable_apks += 1
+
+        if error:
+            global analysis_errors
+            analysis_errors += 1
+
+        if invalid_target_sdk:
+            global target_sdk_too_low
+            target_sdk_too_low += 1
+
+        if no_permission:
+            global no_permissions_declared
+            no_permissions_declared += 1
+
+
 def print_result():
     global analyzed_apks
     global not_analyzable_apks
@@ -72,5 +109,5 @@ def print_result():
     print("\t-\t%d apps raised errors during analysis" % analysis_errors)
 
 if __name__ == '__main__':
-    analyze_results(sys.argv[1])
+    analyze_results_from_json(sys.argv[1])
     print_result()
